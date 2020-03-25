4f33119cb64019d6faa17be1214baaf56d5b94b4
Irene Dovydenko <irenedovydenko@gmail.com>
2020-03-25 11:11:49 +0300

    init

---
+++ trello.py
@@ -0,0 +1,63 @@
+import sys 
+import requests    
+base_url = "https://api.trello.com/1/{}" auth_params = {    
+    'key': "63905fdffebc42360ed0c1bca3f70ef8",    
+    'token': "acfd668e865fd0684169f9b8ad98b6a1043cd2f6985a2512144b30c503a75f3a", }
+board_id = "ljkKrNGy"    
+    
+def read():      
+    # Получим данные всех колонок на доске:      
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()      
+      
+    # Теперь выведем название каждой колонки и всех заданий, которые к ней относятся:      
+    for column in column_data:      
+        print(column['name'])    
+        # Получим данные всех задач в колонке и перечислим все названия      
+        task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()      
+        if not task_data:      
+            print('\t' + 'Нет задач!')      
+            continue      
+        for task in task_data:      
+            print('\t' + task['name'])    
+    
+def create(name, column_name):      
+    # Получим данные всех колонок на доске      
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()      
+      
+    # Переберём данные обо всех колонках, пока не найдём ту колонку, которая нам нужна      
+    for column in column_data:      
+        if column['name'] == column_name:      
+            # Создадим задачу с именем _name_ в найденной колонке      
+            requests.post(base_url.format('cards'), data={'name': name, 'idList': column['id'], **auth_params})      
+	        break  
+    
+def move(name, column_name):    
+    # Получим данные всех колонок на доске    
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()    
+        
+    # Среди всех колонок нужно найти задачу по имени и получить её id    
+    task_id = None    
+    for column in column_data:    
+        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()    
+        for task in column_tasks:    
+            if task['name'] == name:    
+                task_id = task['id']    
+                break    
+        if task_id:    
+            break    
+       
+    # Теперь, когда у нас есть id задачи, которую мы хотим переместить    
+    # Переберём данные обо всех колонках, пока не найдём ту, в которую мы будем перемещать задачу    
+    for column in column_data:    
+        if column['name'] == column_name:    
+            # И выполним запрос к API для перемещения задачи в нужную колонку    
+            requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'], **auth_params})    
+            break    
+    
+if __name__ == "__main__":    
+    if len(sys.argv) <= 2:    
+        read()    
+    elif sys.argv[1] == 'create':    
+        create(sys.argv[2], sys.argv[3])    
+    elif sys.argv[1] == 'move':    
+        move(sys.argv[2], sys.argv[3])  
